08d36e3ecfe2ec4973e022ee77c6fc0a9735c681
Jeffrey <jeffreygrospe@yahoo.com>
2022-09-28 17:45:47 -0400

    Modified Login Controller & Model user
    Added new method in User model

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -10,23 +10,25 @@ class Login extends \app\core\Controller{
 		$this->view('Login/register');
 	}
 
-	public function verify(){
+	public function verifyUser(){
 		// looks for POST 'loginSubmit' request
 		if (isset($_POST['loginSubmit'])){
 			// create a User object
 			$currentUser = new \app\models\User();
 			$username = $_POST['username'];
+			// TODO: hash the password to match the one store in the DB
 			$password = $_POST['password'];
 			// This will return false if no row is found
 			if($currentUser->getUserByUsername($username)){
 				if($currentUser->getUserPasswordByPassword($password)){
-
+					// redirect to the profile page
 				} else {
-					// Invalid password
+					// TODO: Display invalid password
+					
 				}
 			}
 			else{
-				// User does not exists
+				// TODO: Display User does not exists
 			}
 		}
 	}
--- app/models/User.php
+++ app/models/User.php
@@ -2,9 +2,6 @@
     namespace app\models;
 
     class User extends \app\core\Model {
-   
-
-
         public function getUserByUsername($username) {
             $SQL = "SELECT * FROM user WHERE username = :username";
             $STMT = self::$_connection->prepare($SQL);
@@ -14,10 +11,26 @@
         }
 
         public function getUserPasswordByPassword($password){
-            $SQL = "SELECT * FROM user WHERE password = :password";
+            $SQL = "SELECT * FROM user WHERE password_hash = :password";
             $STMT = self::$_connection->prepare($SQL);
             $STMT->execute(['password'=>$username]);
             $STMT->setFetchMode(\PDO::FETCH_CLASS,'app\models\User');
             return $STMT->fetch();
+        }
+        public function getUserPasswordByID($id){
+      
+            $SQL = "SELECT * FROM user WHERE user_id = :id";
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute(['id'=>$username]);
+            $STMT->setFetchMode(\PDO::FETCH_CLASS,'app\models\User');
+            return $STMT->fetch();
+        }
+
+        public function getAllUsers(){
+            $SQL = "SELECT * FROM user";
+            $STMT = self::$_connection->prepare($SQL);
+            $STMT->execute();
+            $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\User');
+            return $STMT->fetchAll();
         }
     }
