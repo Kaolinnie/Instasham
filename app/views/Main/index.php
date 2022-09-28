7f1f89066fbbc29c676e2a1d550bd52bfd877aa5
Kaolin <79109827+Kaolinnie@users.noreply.github.com>
2022-09-27 22:07:14 -0400

    Created instasham logo

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -3,11 +3,28 @@
 	<title>Instasham Home</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="/app/resources/styles/mainStyles.css">
 </head>
 
 <body>
-<p>This is the index of the Main controller.</p>
-
-
+	<header>
+		<img src="/app/resources/images/instashamLogo.png" alt="logo" id="headerLogo">
+		<div class="searchBar">
+			<input type="text" name="search_bar" id="search_bar">
+		</div>
+		<div class="tabLinks">
+			<div class="tabLink homeLink"></div>
+			<div class="tabLink messengerLink"></div>
+			<div class="tabLink createPostLink"></div>
+			<div class="tabLink exploreLink"></div>
+			<div class="tabLink notificationsLink"></div>
+			<div class="tabLink profileLink"></div>
+		</div>
+	</header>
+	<main>
+		/* 
+			add content through php view method depending on the tabLink selected
+		*/
+	</main>
 </body>
 </html>
