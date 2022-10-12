ed47fca863eb6acb86738963ee0073052ec02c6a
Kaolin <79109827+KaolinKiiro@users.noreply.github.com>
2022-10-12 11:30:37 -0400

    Fixing the DB part

--- app/core/Model.php
+++ app/core/Model.php
@@ -5,15 +5,17 @@ class Model{
 	protected static $_connection;
 
 	public function __construct(){
-		// For testing: currently using local DB
-		// $server = 'localhost';//127.0.0.1
-		// $dbname = 'instasham';
-		// $username = 'root';
-		// $password = '';
-		$server = 'jodyplex.ddns.net';//127.0.0.1
+		// local DB
+		$server = 'localhost';//127.0.0.1
 		$dbname = 'instasham';
-		$username = 'instasham';
-		$password = 'eCommerceInstashamDB2022!';
+		$username = 'root';
+		$password = '';
+
+		// my hosted DB which you can access right now if you would like, just uncomment
+		// $server = 'jodyplex.ddns.net';
+		// $dbname = 'instasham';
+		// $username = 'instasham';
+		// $password = 'eCommerceInstashamDB2022!';
 
 		try{
 			self::$_connection = new \PDO("mysql:host=$server;dbname=$dbname",
